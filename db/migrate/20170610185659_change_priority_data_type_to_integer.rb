1fbed5e146d45a8ef008b885322dcdef0f94a8a4
giovanni <giovannilai86@gmail.com>
2017-06-27 11:34:59 +0200

    Fixed db migration for postgres

--- db/migrate/20170610185659_change_priority_data_type_to_integer.rb
+++ db/migrate/20170610185659_change_priority_data_type_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangePriorityDataTypeToInteger < ActiveRecord::Migration
   def change
-  	change_column :todo_items, :priority, :integer
+  	change_column :todo_items, :priority, 'integer USING CAST(priority AS integer)'
   end
 end
